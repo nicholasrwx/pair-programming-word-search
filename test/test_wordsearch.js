9178086ff3fbd584fa89894b64e6be7ab71a3500
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-01-13 22:17:30 +0000

    paired programming solutions

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,59 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['C', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYNC')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if the array is empty", function() {
+    const result = wordSearch([[]], 'ASYNC')
+
+    assert.isUndefined(result);
+  });
+
+  it("should return true if the word is present and reversed vertically", function() {
+    const result = wordSearch([
+      ['C', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['A', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYNC')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and reversed horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
+    if (letters[0].length === 0) {
+        return undefined;
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
-
-module.exports = wordSearch
+    const verticalLetters = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalLetters[i] = [];
+        for (let j = 0; j < letters.length; j++) {
+            if (letters[j]) {
+                verticalLetters[i].push(letters[j][i]);
+            }
+        }
+    }
+    const verticalJoin = verticalLetters.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+  
+    const horizontalReverseJoin = letters.map(ls => ls.reverse().join(''))
+    for (l of horizontalReverseJoin) {
+        if (l.includes(word)) return true
+    }
+    const verticalReverseJoin = verticalLetters.map(ls => ls.reverse().join(''))
+    for (l of verticalReverseJoin) {
+        if (l.includes(word)) return true
+    }
+  
+    return false;
+  }
+  
+  module.exports = wordSearch;
+  
+  
